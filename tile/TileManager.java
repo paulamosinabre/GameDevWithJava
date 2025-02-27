98a0eb3892e2b2031356aaa1d5ae5d809a208288
Paula Bianca Mosinabre <mosinabre.paulabianca@gmail.com>
2025-02-27 21:03:05 +0800

    TileManager.java

--- tile/TileManager.java
+++ tile/TileManager.java
@@ -5,17 +5,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-
 import javax.imageio.ImageIO;
-
 import main.GamePanel;
 
 //kapag inimport mo yung class, makukuha mo yung mga variables nito allowing code reusability
 
 public class TileManager {
 	GamePanel gp;
-	Tile[] tile;
-	int mapTileNum[][];
+	public Tile[] tile;
+	public int mapTileNum[][];
 	
 	public TileManager(GamePanel gp) {
 		
@@ -26,7 +24,7 @@ public class TileManager {
 		getTileImage();
 		loadMap("/map/map.txt");
 	}
-	
+
 	public void getTileImage() {
 		try {
 			tile[0] = new Tile();
@@ -34,9 +32,11 @@ public class TileManager {
 			
 			tile[1] = new Tile();
 			tile[1].image = ImageIO.read(getClass().getResourceAsStream("/background/bush.png"));
+			tile[1].collision = true;
 			
 			tile[2] = new Tile();
 			tile[2].image = ImageIO.read(getClass().getResourceAsStream("/background/chair.png"));
+			tile[2].collision = true;
 			
 			tile[3] = new Tile();
 			tile[3].image = ImageIO.read(getClass().getResourceAsStream("/background/gray.png"));
@@ -44,17 +44,19 @@ public class TileManager {
 			tile[4] = new Tile();
 			tile[4].image = ImageIO.read(getClass().getResourceAsStream("/background/white.png"));
 			
+			tile[5] = new Tile();
+			tile[5].image = ImageIO.read(getClass().getResourceAsStream("/background/brick.png"));
+			tile[5].collision = true;
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void loadMap(String filePath) {
 		try {
-			
 			InputStream is = getClass().getResourceAsStream(filePath);
 			BufferedReader br = new BufferedReader(new InputStreamReader(is)); 
-			
 			
 			int col = 0;
 			int row = 0;
@@ -77,9 +79,7 @@ public class TileManager {
 				
 				if(col == gp.maxWorldCol) {
 					col = 0;
-					//x = 0;
-					row++;
-					//y += gp.tileSize;
+					row++;	
 				}
 			}
 			br.close();
@@ -92,35 +92,31 @@ public class TileManager {
 		int worldCol = 0;
 		int worldRow = 0;
 		
-		
 		while(worldCol < gp.maxWorldCol && worldRow < gp.maxWorldRow) {
 			
 			int tileNum = mapTileNum[worldCol][worldRow];
 			
-			//0*48
+			//0*39
 			int worldX = worldCol * gp.tileSize;
 			int worldY = worldRow * gp.tileSize;
 			int screenX = worldX - gp.player.worldX + gp.player.screenX;
 			int screenY = worldY - gp.player.worldY + gp.player.screenY;
 			
+			 
 			if(worldX + gp.tileSize > gp.player.worldX - gp.player.screenX && 
 					worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
 					worldY + gp.tileSize > gp.player.worldY - gp.player.screenY &&
 					worldY - gp.tileSize < gp.player.worldY + gp.player.screenY ) {
 				g2.drawImage(tile[tileNum].image, screenX, screenY, gp.tileSize,gp.tileSize,null);
 			}
-				
 			
 			worldCol++;
-			
 			
 			if(worldCol == gp.maxWorldCol) {
 				worldCol = 0;
 				worldRow++;
 				
 			}
-			
-		
 		}
 	}
 }
