2306b31875040d8efde9bdb185fab9f9f0d1ce81
Paula Bianca Mosinabre <mosinabre.paulabianca@gmail.com>
2025-02-27 21:01:34 +0800

    Entity.java

--- entity/Entity.java
+++ entity/Entity.java
@@ -1,15 +1,16 @@
 package entity;
 
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
-
-
 
 public class Entity {
     public int worldX, worldY;
     public int speed;
+    
     //Use BufferedImage to access the image. It stores image files 
     public BufferedImage up, down, left, right;
     public String direction;
-    
-    
+    public String obstaclesImg;
+    public Rectangle solidArea;
+    public boolean collisionOn = false;
 }
